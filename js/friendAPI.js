64ea693539dcb80df925ef255e7bce64bd9be620
Kustiche <ChernichnyKustik@mail.ru>
2023-05-21 12:27:55 +0300

    Correction API

--- js/friendAPI.js
+++ js/friendAPI.js
@@ -1,23 +1,18 @@
 import { name, age, photo } from "./view.js";
+const url = 'https://randomuser.me/api/?noinfo';
 
 export function friendAPI() {
-  const url = 'https://randomuser.me/api/?noinfo';
 
   fetch(url)
     .then((response) => response.json())
     .then((data) => data.results[0])
-    .then((resultPhoto) => resultPhoto.picture)
-    .then((questionnairePhoto) => photo.src = questionnairePhoto.large)
+    .then((result) => {
+      const resultAge = result.dob;
+      const resultName = result.name;
+      const resultPhoto = result.picture
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => data.results[0])
-    .then((resultDob) => resultDob.dob)
-    .then((questionnaireAge) => age.textContent = questionnaireAge.age + ' y. o.')
-
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => data.results[0])
-    .then((resultName) => resultName.name)
-    .then((questionnaireName) => name.textContent = questionnaireName.first + ' ' + questionnaireName.title + ' ' + questionnaireName.last)
+      name.textContent = resultName.first + ' ' + resultName.title + ' ' + resultName.last;
+      age.textContent = resultAge.age;
+      photo.src = resultPhoto.large;
+    })
 };
